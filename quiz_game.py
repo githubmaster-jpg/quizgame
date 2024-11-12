5f29a8f46b1e0fb7f6d97547b44339f0d09a82d8
githubmaster-jpg <149624283+githubmaster-jpg@users.noreply.github.com>
2024-11-12 23:57:04 +0200

    Update quiz_game.py
    Added ability to play multiple levels and check for more complex answers. 
    Improved present_question and check_answer documentation, but removed main documentation. 
    Added temporary level lists and questions for testing.

--- quiz_game.py
+++ quiz_game.py
@@ -1,11 +1,23 @@
+import re
 level_1 = [['What is 1+1?', '2', 'TWO'],
           ['How many colors does a rainbow have?', '7', 'SEVEN'],
           ['How many meters are in a kilometer?', '1000', 'THOUSAND'],
-          ['Which is the smallest month of the year?', 'FEBRUARY', 'FEBRUARY'],
-          ['What animal is known as the king of the jungle?', 'LION', 'LION']]
+          ['Which is the smallest month of the year?', 'FEBRUARY'],
+          ['What animal is known as the king of the jungle?', 'LION'],
+          ['What are the three primary colors of light?', 'REDGREENBLUE', 'REDBLUEGREEN', 'BLUEREDGREEN', 'BLUEGREENRED', 'GREENREDBLUE','GREENBLUERED']
+          ]
 
-def present_question(level,number):
-    """ Asks the user a question from the level selected. Currently supports one level selection.
+level_2 = [['What is 3*5', '15', 'FIFTEEN']
+           ]
+
+level_3 = [['What is the capital of Albania?', 'TIRANA']
+           ]
+
+levels = [level_1, level_2, level_3]
+
+
+def present_question(level:list, number:int) -> str:
+    """ Asks the user a question from the level selected.
 
     Parameters
     ----------
@@ -24,11 +36,11 @@ def present_question(level,number):
     return answer
 
 
-def check_answer(level,answer,number,correct):
-    """ Checks the user's answer and compares it to the correct one. Currently supports one level selection.
+def check_answer(level:list, answer:str, number:int, correct:int) -> int:
+    """ Checks the user's answer and compares it to the correct one.
 
-    Turns the user's answer to capital letters to match the answer list. Try block prevents any errors from stopping the code.
-    Answer gets compared to both numerical and text based answer in case the answer should support both. As long as one of the
+    Turns the user's answer to capital letters to match the answer list. Removes 'and' and other special characters from answer.
+    Try block prevents any errors from stopping the code. Answer gets compared to the answers in list. As long as one of the
     correct answers is detected, the answer will be marked as correct. If a wrong or an error causing answer is submitted by
     the user, the answer will be marked incorrect.
 
@@ -50,8 +62,10 @@ def check_answer(level,answer,number,correct):
     """
 
     answer = answer.upper()
+    answer = answer.replace(' AND ', '')
+    answer = re.sub(r'[^a-zA-Z0-9]','',answer)
     try:
-        if answer == level[number-1][1] or answer == level[number-1][2]:
+        if answer in level[number-1]:
             print(f'Good job!\nCorrect amount of answers: {correct+1}')
             return 1
         else:
@@ -64,23 +78,25 @@ def check_answer(level,answer,number,correct):
 
 def main():
     """ Enables the quiz game to run.
-
-    The 'correct' and 'number' variables increase as the game progresses. There is no way to decrease
-    them by the time of this update. The 'questions' takes the len of the list, allowing it to run 
-    through all the questions in a level container. While loop is used to one by one present questions
-    and check for the correct answers. The amount of correct answers gets printed to the user at the 
-    end of the game.
     """
-
-    correct = 0  
-    number = 1
-    questions = len(level_1)
-    while number <= questions:
-        answer=present_question(level_1, number)
-        correct_answer = check_answer(level_1,answer,number,correct)
-        correct = correct + correct_answer
-        number += 1
-    print(f'Total amount of correct answers: {correct}')
+    
+    game_round = 0
+    all_levels = len(levels)
+    all_correct = 0  
+    while game_round+1 <= all_levels:
+        correct = 0
+        number = 1
+        questions = len(levels[game_round])
+        print(f'Quiz Level {game_round+1}:')
+        while number <= questions:
+            answer=present_question(levels[game_round], number)
+            correct_answer = check_answer(levels[game_round],answer,number,correct)
+            correct = correct + correct_answer
+            number += 1
+        #print(f'Amount of correct answers in level: {correct}') # prints number of correct answers after finishing a level, but the check function prints it after every correct answer at this moment
+        all_correct = all_correct + correct
+        game_round += 1
+    print(f'Total amount of correct answers: {all_correct}')
     
 
 if __name__ == "__main__":
